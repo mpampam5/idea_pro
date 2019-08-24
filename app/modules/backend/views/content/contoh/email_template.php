745ea2c3b818f6e6ad34423b0427ebf0aa12fe9b
mpampam5 <mpampam5@gmail.com>
2019-08-25 02:37:55 +0700

    email send

--- app/modules/backend/views/content/contoh/email_template.php
+++ app/modules/backend/views/content/contoh/email_template.php
@@ -86,7 +86,7 @@ a h1{
                   <td width="560" valign="top" align="center" style="padding:0;Margin:0;">
                    <table width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;">
                      <tr style="border-collapse:collapse;">
-                      <td align="center" style="padding:0;Margin:0;"><h1 style="Margin:0;line-height:31px;font-size:26px;color:#fff!important;"><strong>IDEA DIGITAL INDONESIA</strong></h1></td>
+                      <td align="center" style="padding:0;Margin:0;"><h1 style="Margin:0;line-height:31px;font-size:26px;color:#fff!important;"><strong>MPAMPAM DOT COM</strong></h1></td>
                      </tr>
                    </table></td>
                  </tr>
