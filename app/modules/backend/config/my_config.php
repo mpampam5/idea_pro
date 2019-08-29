0736d7dfcb727a073e69ba9b97ca05e4ea9a7970
mpampam5 <mpampam5@gmail.com>
2019-08-29 01:04:00 -0700

    ideaaaa

--- app/modules/backend/config/my_config.php
+++ app/modules/backend/config/my_config.php
@@ -1,7 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$config['author'] = "mpampam";
 
 
 $config['encrypt_gue']['encryption_key'] = "mpampam595@@@@";
@@ -10,6 +9,6 @@ $config['encrypt_gue']['encryption_mechanism'] = "aes-256-cbc";
 
 //konfigurasi email
 $config['email']      = "info@mpampam.com";
-$config['password']   = "dsdasasd";
+$config['password']   = "zzzzz";
 $config['smtp_host']  = "ssl://mail.mpampam.com";
 $config['smtp_port']  = "465";
