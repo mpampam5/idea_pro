4f8eae4bc7b75658e3ca3c805525e629cbafcfb1
mpampam5 <mpampam5@gmail.com>
2019-09-02 17:53:23 +0700

    Belum fix

--- app/modules/backend/views/content/withdraw/form_add_withdraw.php
+++ app/modules/backend/views/content/withdraw/form_add_withdraw.php
@@ -3,8 +3,12 @@
       <h4 class="text-primary">BALANCE : Rp. <?=format_rupiah($balance)?></h4>
       <p class="alert alert-warning">Harap Hubungi admin jika ada perubahan mengenai data Rekening Bank anda.</p>
       <p>Silahkan Masukkan Nominal yang akan anda Withdraw.</p>
-      <p>Minimal Withdraw <b>Rp.<?=format_rupiah(config_all('min_withdraw'))?></b></p>
-      <p>Maximal Withdraw <b>Rp.<?=format_rupiah(config_all('max_withdraw'))?></b></p>
+      <?php if (config_all('min_withdraw')!=0): ?>
+        <p class="text-primary">Minimal Withdraw <b>Rp.<?=format_rupiah(config_all('min_withdraw'))?></b></p>
+      <?php endif; ?>
+      <?php if (config_all('max_withdraw')!=0): ?>
+        <p class="text-primary">Maximal Withdraw <b>Rp.<?=format_rupiah(config_all('max_withdraw'))?></b></p>
+      <?php endif; ?>
       <?php else: ?>
         <p>Anda Yakin Ingin Membatalkan Withdraw Anda?</p>
     <?php endif; ?>
